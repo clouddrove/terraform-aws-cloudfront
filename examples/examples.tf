7d9c2440dda2a0508474050de4cc3e14ecb17a3e
Sohan <31764946+sohanyadav@users.noreply.github.com>
2019-05-15 19:25:00 +0530

    Update examples.tf

--- examples/examples.tf
+++ examples/examples.tf
@@ -11,7 +11,6 @@ module "cdn" {
   origin_id                =    "s3-secure-bucket"
   viewer_protocol_policy   =    "redirect-to-https"
   compress                 =    "false"
-  trusted_signers          = [65665959196, ] #aws account number
   allowed_methods               =  ["GET", "HEAD" ]
   acm_certificate_arn       =  "arn:aws:acm:us-east-1:870902197131:certificate/d67e427c-ec3b-4ecb-9f4a-xxxxx"
 
