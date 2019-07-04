c03c2359789f20ecd3b85c66871d04cd7c366634
Sohan Yadav <sohan@clouddrove.com>
2019-07-04 20:55:03 +0530

    update_readme

--- _example/basic/example.tf
+++ _example/basic/example.tf
@@ -2,23 +2,27 @@ provider "aws" {
   region = "us-east-1"
 }
 module "s3_bucket" {
-  source         = "git::https://github.com/clouddrove/terraform-aws-s3.git"
-  name           = "secure-bucket-cdn"
-  region         = "us-east-1"
-  application    = "clouddrove"
-  environment    = "dev"
-  label_order    = ["environment", "name", "application"]
+  source = "git::https://github.com/clouddrove/terraform-aws-s3.git"
+
+  name        = "secure-bucket-cdn"
+  region      = "us-east-1"
+  application = "clouddrove"
+  environment = "test"
+  label_order = ["environment", "name", "application"]
+
   versioning     = true
   acl            = "private"
   bucket_enabled = true
 }
 
 module "acm" {
-  source               = "git::https://github.com/clouddrove/terraform-aws-acm.git"
-  application          = "clouddrove"
-  environment          = "dev"
-  name                 = "certificate"
-  label_order          = ["environment", "name", "application"]
+  source = "git::https://github.com/clouddrove/terraform-aws-acm.git"
+
+  name        = "certificate"
+  application = "clouddrove"
+  environment = "test"
+  label_order = ["environment", "name", "application"]
+
   domain_name          = "clouddrove.com"
   validation_method    = "EMAIL"
   validate_certificate = false
@@ -29,9 +33,9 @@ module "cdn" {
 
   name        = "basic-cdn"
   application = "clouddrove"
+  environment = "test"
   label_order = ["environment", "name", "application"]
 
-  environment            = "test"
   aliases                = ["clouddrove.com"]
   bucket_name            = module.s3_bucket.s3_default_id[0]
   viewer_protocol_policy = "redirect-to-https"
