682fe368cde531494d9a26d3570e8ee8348845eb
Sohan Yadav <sohan@clouddrove.com>
2019-07-30 16:10:21 +0530

    update-stachure

--- _example/bucket-secure/example.tf
+++ _example/bucket-secure/example.tf
@@ -30,7 +30,7 @@ module "acm" {
 }
 
 module "cdn" {
-  source = "git::https://github.com/clouddrove/terraform-aws-cloudfront-cdn.git"
+  source = "./../../"
 
   name        = "secure-cdn"
   application = "clouddrove"
