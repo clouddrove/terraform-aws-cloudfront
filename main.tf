42e5b0eeb09d26e3c8b55f773d0d493fd6d83e92
Nikita Dugar <nikita@clouddrove.com>
2019-08-02 17:54:16 +0530

    make readme and run terratest

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@
 ## Copyright @ CloudDrove. All Right Reserved.
 
 #Module      : label
-#Description : This terraform module is designed to generate consistent label names and 
-#              tags for resources. You can use terraform-labels to implement a strict 
+#Description : This terraform module is designed to generate consistent label names and
+#              tags for resources. You can use terraform-labels to implement a strict
 #              naming convention.
 module "labels" {
   source = "git::https://github.com/clouddrove/terraform-labels.git"
