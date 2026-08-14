107414eed1f8efb2d95ee13d11d86f173b7d3391
hahirwar-cd <himanshu.ahirwar@clouddrove.com>
2026-08-14 17:22:50 +0530

    fix(variable): remove repository variable validation

--- variables.tf
+++ variables.tf
@@ -16,12 +16,6 @@ variable "repository" {
   type        = string
   default     = "https://github.com/clouddrove/terraform-aws-cloudfront-cdn"
   description = "Terraform current module repo"
-
-  validation {
-    # regex(...) fails if it cannot find a match
-    condition     = can(regex("^https://", var.repository))
-    error_message = "The module-repo value must be a valid Git repo link."
-  }
 }
 
 variable "environment" {
